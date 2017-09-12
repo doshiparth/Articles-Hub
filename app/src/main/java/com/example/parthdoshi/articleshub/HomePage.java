21d3aa94e61f81a9eb5d93b1998275b02e54593c
Parth Doshi <doshiparth007@gmail.com>
2017-09-12 23:15:21 +0530

    Added a "floating action button" on the home for giving access to "WriteArticlePage" Added 4 transition files in "res\anim" for future use

--- app/src/main/java/com/example/parthdoshi/articleshub/HomePage.java
+++ app/src/main/java/com/example/parthdoshi/articleshub/HomePage.java
@@ -3,7 +3,9 @@ package com.example.parthdoshi.articleshub;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -41,14 +43,17 @@ public class HomePage extends AppCompatActivity
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+
+                Intent myIntent = new Intent(HomePage.this, WriteArticlePage.class);
+                startActivity(myIntent);
             }
-        });*/
+        });
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
